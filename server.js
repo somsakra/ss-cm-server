90bb69925123a53c9c7948bb0bb120208460618a
BigDataAdm020 <BigDataAdm020@NB3400020.oa.bbl>
2023-04-03 09:56:31 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(require('./controller'))
+app.use('/images', express.static('images'))
+
+
+const PORT = process.env.PORT || 1150
+const ENV = process.env.ENV || 'development'
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on env ${ENV}`);
+    console.log('Press Ctrl+C to quit.');
+})
